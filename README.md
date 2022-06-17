469fbd6bacbb9545df74635b60dd5271b21f86b5
yusuf-cirak <rewal34@gmail.com>
2022-06-17 21:33:44 +0300

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ECommerseAngularClient
+# ECommecseAngularClient
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.1.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/etrade-angular-client'),
+      dir: require('path').join(__dirname, './coverage/ecommerce-angular-client'),
       subdir: '.',
       reporters: [
         { type: 'html' },
